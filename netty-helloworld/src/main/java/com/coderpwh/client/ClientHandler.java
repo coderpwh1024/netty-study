120a534a8d93ff9285674172a89333fc0d62e880
coderpwh <pengwenhao@aliyun.com>
2023-11-28 15:50:35 +0800

    feat:Init client

---
+++ netty-helloworld/src/main/java/com/coderpwh/client/ClientHandler.java
@@ -0,0 +1,25 @@
+package com.coderpwh.client;
+
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+@ChannelHandler.Sharable
+public class ClientHandler extends SimpleChannelInboundHandler<String> {
+
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, String msg) throws Exception {
+
+        System.out.println("msg:" + msg);
+    }
+
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+
+}
