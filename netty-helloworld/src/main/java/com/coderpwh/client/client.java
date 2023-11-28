c68c51902955b8e687a34c59a0ca3225f75c2ce9
coderpwh <pengwenhao@aliyun.com>
2023-11-28 16:52:03 +0800

    fix:update client

--- netty-helloworld/src/main/java/com/coderpwh/client/client.java
+++ netty-helloworld/src/main/java/com/coderpwh/client/client.java
@@ -5,9 +5,10 @@ import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.sctp.nio.NioSctpServerChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 
@@ -16,43 +17,44 @@ public class client {
     public static void main(String[] args) {
 
         EventLoopGroup group = new NioEventLoopGroup();
-
         try {
             Bootstrap b = new Bootstrap();
             b.group(group)
-                    .channel(NioSctpServerChannel.class).handler(new ClientHandler());
+                    .channel(NioSocketChannel.class)
+                    .handler(new ClientInitializer());
+            Channel ch = b.connect("127.0.0.1",8888).sync().channel();
 
-            Channel ch = b.connect("127.0.0.1", 8888).sync().channel();
 
             ChannelFuture lastWriteFuture = null;
-
             BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-
-            for (; ; ) {
+            for (;;) {
                 String line = in.readLine();
-
                 if (line == null) {
                     break;
                 }
 
+                // Sends the received line to the server.
                 lastWriteFuture = ch.writeAndFlush(line + "\r\n");
 
+                // If user typed the 'bye' command, wait until the server closes
+                // the connection.
                 if ("bye".equals(line.toLowerCase())) {
                     ch.closeFuture().sync();
                     break;
                 }
             }
 
+            // Wait until all messages are flushed before closing the channel.
             if (lastWriteFuture != null) {
                 lastWriteFuture.sync();
             }
-
-        } catch (Exception e) {
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
             group.shutdownGracefully();
         }
-
 
     }
 
