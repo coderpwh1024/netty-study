fa80f6043be765286c5000d8ad08fdfce1e74fa0
coderpwh <pengwenhao@aliyun.com>
2023-11-28 14:43:29 +0800

    fix: update ServerInitializer

--- netty-helloworld/src/main/java/com/coderpwh/server/ServerInitializer.java
+++ netty-helloworld/src/main/java/com/coderpwh/server/ServerInitializer.java
@@ -1,9 +1,13 @@
 package com.coderpwh.server;
 
+import com.sun.corba.se.impl.interceptors.PICurrent;
 import com.sun.corba.se.spi.activation.ServerHelper;
 import com.sun.xml.internal.ws.message.StringHeader;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.Delimiters;
 import io.netty.handler.codec.string.StringDecoder;
 
 public class ServerInitializer extends ChannelInitializer<SocketChannel> {
@@ -12,11 +16,23 @@ public class ServerInitializer extends ChannelInitializer<SocketChannel> {
 
     private static final StringDecoder ENCODER = new StringDecoder();
 
-//    private static final ServerHandler SERVER_HANDLER = new ServerHandler();
+    private static final ServerHandler SERVER_HANDLER = new ServerHandler();
 
 
+    /***
+     * 初始化通道
+     * @param socketChannel
+     * @throws Exception
+     */
     @Override
     protected void initChannel(SocketChannel socketChannel) throws Exception {
+
+        ChannelPipeline pipeline = socketChannel.pipeline();
+
+        pipeline.addLast(new DelimiterBasedFrameDecoder(8192, Delimiters.lineDelimiter()));
+        pipeline.addLast(DECODER);
+        pipeline.addLast(ENCODER);
+        pipeline.addLast(SERVER_HANDLER);
 
     }
 
