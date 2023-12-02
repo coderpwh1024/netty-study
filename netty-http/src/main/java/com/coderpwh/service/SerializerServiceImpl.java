d600cc38e43741de4428db0edbabc9b60fe9f245
coderpwh <pengwenhao@aliyun.com>
2023-12-02 22:15:39 +0800

    fix:update SerializerServiceImpl

--- netty-http/src/main/java/com/coderpwh/service/SerializerServiceImpl.java
+++ netty-http/src/main/java/com/coderpwh/service/SerializerServiceImpl.java
@@ -1,4 +1,16 @@
 package com.coderpwh.service;
 
-public class SerializerServiceImpl {
+import com.alibaba.fastjson.JSON;
+
+public class SerializerServiceImpl implements SerializerService {
+
+    @Override
+    public byte[] serialize(Object object) {
+        return JSON.toJSONBytes(object);
+    }
+
+    @Override
+    public <T> T deserialize(Class<T> clazz, byte[] bytes) {
+        return JSON.parseObject(bytes, clazz);
+    }
 }
