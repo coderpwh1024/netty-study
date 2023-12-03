99030b1820b3effb2d0832c6664700319ff16f70
coderpwh <pengwenhao@aliyun.com>
2023-12-03 15:40:06 +0800

    fix:update HttpHelloWorldServerHandler

--- netty-http/src/main/java/com/coderpwh/Initializer/HttpHelloWorldServerHandler.java
+++ netty-http/src/main/java/com/coderpwh/Initializer/HttpHelloWorldServerHandler.java
@@ -1,14 +1,21 @@
 package com.coderpwh.Initializer;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.coderpwh.entity.User;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
-import io.netty.handler.codec.http.FullHttpRequest;
-import io.netty.handler.codec.http.HttpHeaders;
-import io.netty.handler.codec.http.HttpObject;
-import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.*;
 import io.netty.util.AsciiString;
+import org.apache.commons.codec.CharEncoding;
+import org.apache.commons.codec.Charsets;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.print.DocFlavor;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 public class HttpHelloWorldServerHandler extends SimpleChannelInboundHandler<HttpObject> {
 
@@ -35,7 +42,84 @@ public class HttpHelloWorldServerHandler extends SimpleChannelInboundHandler<Htt
 
 
     @Override
-    protected void channelRead0(ChannelHandlerContext channelHandlerContext, HttpObject httpObject) throws Exception {
+    protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
+
+        User user = new User();
+        user.setUserName("coderpwh");
+        user.setDate(new Date());
+
+        if (msg instanceof HttpRequest) {
+            request = (HttpRequest) msg;
+            headers = request.headers();
+            String uri = request.uri();
+
+            logger.info("http uri:" + uri);
+
+            if (uri.equals(FAVICON_ICO)) {
+                return;
+            }
+
+            HttpMethod method = request.method();
+
+            if (method.equals(HttpMethod.GET)) {
+                QueryStringDecoder queryStringDecoder = new QueryStringDecoder(uri, Charsets.toCharset(CharEncoding.UTF_8));
+                Map<String, List<String>> uriAttributes = queryStringDecoder.parameters();
+
+                for (Map.Entry<String, List<String>> attr : uriAttributes.entrySet()) {
+                    for (String attrVal : attr.getValue()) {
+                        logger.info(attr.getKey() + "=" + attrVal);
+                    }
+                }
+                user.setMethod("get");
+            } else if (method.equals(HttpMethod.POST)) {
+
+                fullHttpRequest = (FullHttpRequest) msg;
+
+            }
+
+
+        }
+
 
     }
+
+
+    /***
+     * 处理常用的Content-type
+     */
+    public void dealWithContentType() {
+        String contentType = getContentType();
+
+        if (contentType.equals("application/json")) {
+            String jsonStr = fullHttpRequest.content().toString(Charsets.toCharset(CharEncoding.UTF_8));
+            JSONObject obj = JSON.parseObject(jsonStr);
+
+            for (Map.Entry<String, Object> item : obj.entrySet()) {
+                logger.info(item.getKey() + "=" + item.getValue().toString());
+            }
+        } else if (contentType.equals("application/x-www-form-urlencoded")) {
+            //方式一：使用 QueryStringDecoder
+            String jsonStr = fullHttpRequest.content().toString(Charsets.toCharset(CharEncoding.UTF_8));
+            QueryStringDecoder queryDecoder = new QueryStringDecoder(jsonStr, false);
+            Map<String, List<String>> uriAttributes = queryDecoder.parameters();
+            for (Map.Entry<String, List<String>> attr : uriAttributes.entrySet()) {
+                for (String attrVal : attr.getValue()) {
+                    logger.info(attr.getKey() + "=" + attrVal);
+                }
+            }
+        } else if (contentType.equals("multipart/form-data")) {
+
+        } else {
+
+        }
+    }
+
+
+    public String getContentType() {
+        String typeStr = headers.get("Content-Type").toString();
+        String[] list = typeStr.split(";");
+        return list[0];
+    }
+
+
 }
