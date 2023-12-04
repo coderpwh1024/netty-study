c11c918b91a69c21c74a117dbcc00f42f9f18d41
coderpwh <pengwenhao@aliyun.com>
2023-12-04 16:10:06 +0800

    feat:Init NettyServer

---
+++ protobuf/src/main/java/com/coderpwh/server/NettyServer.java
@@ -0,0 +1,36 @@
+package com.coderpwh.server;
+
+
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service("nettyServer")
+@Slf4j
+public class NettyServer {
+
+
+    @Value("${server.bind_port}")
+    private Integer port;
+
+    @Value("${server.netty.boss_group_thread_count}")
+    private Integer bossGroupThreadCount;
+
+    @Value("${server.netty.worker_group_thread_count}")
+    private Integer workerGroupThreadCount;
+
+    @Value("${server.netty.leak_detector_level}")
+    private String leakDatectorLevel;
+
+    @Value("${server.netty.max_payload_size}")
+    private Integer maxPayloadSize;
+
+    private ChannelFuture channelFuture;
+
+    private EventLoopGroup bossGroup;
+
+    private EventLoopGroup workerGroup;
+
+}
