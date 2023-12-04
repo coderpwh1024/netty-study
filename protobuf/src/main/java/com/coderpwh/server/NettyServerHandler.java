44ff0674b593f6f6e245cf4cee4083d7664e0eb8
coderpwh <pengwenhao@aliyun.com>
2023-12-04 15:27:34 +0800

    fix:update NettyServerHandler

--- protobuf/src/main/java/com/coderpwh/server/NettyServerHandler.java
+++ protobuf/src/main/java/com/coderpwh/server/NettyServerHandler.java
@@ -1,7 +1,10 @@
 package com.coderpwh.server;
 
+import com.coderpwh.proto.UserMsg;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.atomic.AtomicInteger;
@@ -22,10 +25,55 @@ public class NettyServerHandler extends ChannelInboundHandlerAdapter {
     private AtomicInteger count = new AtomicInteger(1);
 
 
-    public void channelActive(ChannelHandlerContext context) {
+    /***
+     *  通信链接
+     * @param context
+     * @throws Exception
+     */
+    @Override
+    public void channelActive(ChannelHandlerContext context) throws Exception {
         log.info("链接的客户端地址:{}", context.channel().remoteAddress());
 
-//        UserMsg.User user =
+        UserMsg.User user = UserMsg.User.newBuilder().setId(1).setAge(24).setName("coderpwh").setState(0).build();
+        context.writeAndFlush(user);
+        super.channelActive(context);
+
+    }
+
+
+    /***
+     * 超时处理
+     * @param context
+     * @param obj
+     * @throws Exception
+     */
+    public void userEventTriggered(ChannelHandlerContext context, Object obj) throws Exception {
+        if (obj instanceof IdleStateEvent) {
+            IdleStateEvent event = (IdleStateEvent) obj;
+
+            if (IdleState.READER_IDLE.equals(event.state())) {
+                log.info("已经5秒没有接收到客户端的信息");
+                if (idle_count.get() > 1) {
+                    log.info("关闭这个不活跃的channel");
+                    context.channel().close();
+                }
+                idle_count.getAndIncrement();
+            }
+        } else {
+            super.userEventTriggered(context, obj);
+        }
+    }
+
+
+    /***
+     * 异常处理
+     * @param context
+     * @param cause
+     */
+    @Override
+    public void exceptionCaught(ChannelHandlerContext context, Throwable cause) {
+        cause.printStackTrace();
+        context.close();
 
     }
 
