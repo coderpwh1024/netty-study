8cdf9f6181a7bf9365fcd65d39541aaff3e06545
coderpwh <pengwenhao@aliyun.com>
2023-12-05 13:29:13 +0800

    feat:Init NettyClientHandler

---
+++ protobuf/src/main/java/com/coderpwh/client/NettyClientHandler.java
@@ -0,0 +1,14 @@
+package com.coderpwh.client;
+
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import lombok.extern.slf4j.Slf4j;
+
+@ChannelHandler.Sharable
+@Slf4j
+public class NettyClientHandler extends ChannelInboundHandlerAdapter {
+
+
+
+
+}
