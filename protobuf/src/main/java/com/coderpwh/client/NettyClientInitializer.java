a8ff34b7e68de529affdaa9f85910826a75816dd
coderpwh <pengwenhao@aliyun.com>
2023-12-05 13:27:09 +0800

    fix:update NettyCilentInitializer

---
+++ protobuf/src/main/java/com/coderpwh/client/NettyClientInitializer.java
@@ -0,0 +1,43 @@
+package com.coderpwh.client;
+
+import com.coderpwh.proto.UserMsg;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.protobuf.ProtobufDecoder;
+import io.netty.handler.codec.protobuf.ProtobufEncoder;
+import io.netty.handler.codec.protobuf.ProtobufVarint32FrameDecoder;
+import io.netty.handler.codec.protobuf.ProtobufVarint32LengthFieldPrepender;
+import io.netty.handler.timeout.IdleStateHandler;
+
+import java.util.concurrent.TimeUnit;
+
+public class NettyClientInitializer extends ChannelInitializer<SocketChannel> {
+
+
+    /***
+     * 初始化通道
+     * @param socketChannel
+     * @throws Exception
+     */
+    @Override
+    protected void initChannel(SocketChannel socketChannel) throws Exception {
+
+        ChannelPipeline pipeline = socketChannel.pipeline();
+
+        pipeline.addLast(new IdleStateHandler(0, 4, 0, TimeUnit.SECONDS));
+
+        pipeline.addLast(new ProtobufVarint32FrameDecoder());
+        pipeline.addLast(new ProtobufDecoder(UserMsg.User.getDefaultInstance()));
+        pipeline.addLast(new ProtobufVarint32LengthFieldPrepender());
+        pipeline.addLast(new ProtobufEncoder());
+
+        // TODO List 待实现
+
+//        pipeline.addLast("")
+
+
+    }
+
+
+}
