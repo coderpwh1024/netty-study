ace53b3e0c0e79421886ee9365631c08a00dc8c1
coderpwh <pengwenhao@aliyun.com>
2023-12-08 15:20:50 +0800

    fix:update DeviceDao

--- netty-mysql/src/main/java/com/coderpwh/device/DeviceDao.java
+++ netty-mysql/src/main/java/com/coderpwh/device/DeviceDao.java
@@ -3,7 +3,7 @@ package com.coderpwh.device;
 import com.coderpwh.Dao;
 import com.coderpwh.domain.Device;
 
-public interface DeviceDao extends Dao<DeviceDao> {
+public interface DeviceDao extends Dao<Device> {
 
 
     /***
--- netty-mysql/src/main/java/com/coderpwh/device/DeviceServiceImpl.java
+++ netty-mysql/src/main/java/com/coderpwh/device/DeviceServiceImpl.java
@@ -1,14 +1,15 @@
 package com.coderpwh.device;
 
 import com.coderpwh.domain.Device;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 @Service
 public class DeviceServiceImpl implements DeviceService {
 
 
-    @Autowired
+    @Resource
     private DeviceDao deviceDao;
 
 
