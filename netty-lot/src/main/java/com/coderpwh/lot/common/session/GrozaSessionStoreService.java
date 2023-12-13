819346133de328553e4f66cd46e753d4384e65b0
coderpwh <pengwenhao@aliyun.com>
2023-12-13 22:49:14 +0800

    fix:update GrozaSessionStoreService

--- netty-lot/src/main/java/com/coderpwh/lot/common/session/GrozaSessionStoreService.java
+++ netty-lot/src/main/java/com/coderpwh/lot/common/session/GrozaSessionStoreService.java
@@ -3,15 +3,35 @@ package com.coderpwh.lot.common.session;
 public interface GrozaSessionStoreService {
 
 
+    /**
+     * 存储会话
+     *
+     * @param clientId
+     * @param sessionStore
+     */
     void put(String clientId, SessionStore sessionStore);
 
 
+    /***
+     * 获取会话
+     * @param clientId
+     * @return
+     */
     SessionStore get(String clientId);
 
 
+    /***
+     * 判断 clientId是否存在
+     * @param clientId
+     * @return
+     */
     boolean containKey(String clientId);
 
 
+    /***
+     * 删除会话
+     * @param clientId
+     */
     void remove(String clientId);
 
 }
