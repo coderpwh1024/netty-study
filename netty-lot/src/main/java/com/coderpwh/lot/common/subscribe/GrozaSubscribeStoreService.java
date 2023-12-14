91bfaea563d7507a05ed55dc6351668a9ef5eae2
coderpwh <pengwenhao@aliyun.com>
2023-12-14 14:09:04 +0800

    fix:update GrozaSubscribeStoreService

--- netty-lot/src/main/java/com/coderpwh/lot/common/subscribe/GrozaSubscribeStoreService.java
+++ netty-lot/src/main/java/com/coderpwh/lot/common/subscribe/GrozaSubscribeStoreService.java
@@ -1,9 +1,43 @@
 package com.coderpwh.lot.common.subscribe;
 
 
+import java.util.List;
+
 public interface GrozaSubscribeStoreService {
 
 
+    /***
+     * 存储订阅
+     * @param topicFilter
+     * @param subscribeStore
+     */
+    void put(String topicFilter, SubscribeStore subscribeStore);
+
+
+    /**
+     * 删除订阅
+     *
+     * @param topicFilter
+     * @param clientId
+     */
+    void remove(String topicFilter, String clientId);
+
+
+    /**
+     * 删除clientId的订阅
+     *
+     * @param clientId
+     */
+    void removeForClient(String clientId);
+
+
+    /**
+     * 获取订阅储集
+     *
+     * @param topic
+     * @return
+     */
+    List<SubscribeStore> search(String topic);
 
 
 }
