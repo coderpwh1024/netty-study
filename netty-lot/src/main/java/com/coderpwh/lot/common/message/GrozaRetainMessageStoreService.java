ce545093bf686ebc650cd85b372787c62d14b0c7
coderpwh <pengwenhao@aliyun.com>
2023-12-14 15:56:20 +0800

    fix:GrozaRetainMessageStoreService

--- netty-lot/src/main/java/com/coderpwh/lot/common/message/GrozaRetainMessageStoreService.java
+++ netty-lot/src/main/java/com/coderpwh/lot/common/message/GrozaRetainMessageStoreService.java
@@ -1,4 +1,41 @@
 package com.coderpwh.lot.common.message;
 
+import java.util.List;
+
 public interface GrozaRetainMessageStoreService {
+
+
+    /***
+     * 存储消息
+     * @param topic
+     * @param retainMessageStore
+     */
+    void put(String topic, RetainMessageStore retainMessageStore);
+
+
+    /***
+     *  获取retain消息
+     * @param topic
+     * @return
+     */
+    RetainMessageStore get(String topic);
+
+
+    /***
+     * 删除topic
+     * @param topic
+     */
+    void remove(String topic);
+
+
+    /***
+     * 判断是否包含topic
+     * @param topic
+     * @return
+     */
+    boolean containsKey(String topic);
+
+
+    List<RetainMessageStore> search(String topicFilter);
+
 }
