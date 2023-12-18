ebb2f3b4c4abc9ee013ad325a9f67ba7892fd2f6
coderpwh <pengwenhao@aliyun.com>
2023-12-18 15:31:03 +0800

    fix:update GrozaDupPubRelMessageCache

--- netty-lot/src/main/java/com/coderpwh/lot/store/cache/GrozaDupPubRelMessageCache.java
+++ netty-lot/src/main/java/com/coderpwh/lot/store/cache/GrozaDupPubRelMessageCache.java
@@ -18,11 +18,24 @@ public class GrozaDupPubRelMessageCache {
     private RedisTemplate<String, Serializable> redisTemplate;
 
 
+    /***
+     * 存储key
+     * @param clientId
+     * @param messageId
+     * @param dupPubRelMessageStore
+     * @return
+     */
     public DupPubRelMessageStore put(String clientId, Integer messageId, DupPubRelMessageStore dupPubRelMessageStore) {
         redisTemplate.opsForHash().put(CACHE_PRE + clientId, String.valueOf(messageId), dupPubRelMessageStore);
         return dupPubRelMessageStore;
     }
 
+
+    /***
+     * 获取key
+     * @param clientId
+     * @return
+     */
     public ConcurrentHashMap<Integer, DupPubRelMessageStore> get(String clientId) {
         ConcurrentHashMap<Integer, DupPubRelMessageStore> map = new ConcurrentHashMap<>();
 
@@ -35,6 +48,37 @@ public class GrozaDupPubRelMessageCache {
             });
         }
         return map;
+    }
+
+
+    /**
+     * 包含key
+     *
+     * @param clientId
+     * @return
+     */
+    public boolean containsKey(String clientId) {
+        return redisTemplate.hasKey(CACHE_PRE + clientId);
+    }
+
+
+    /**
+     * 删除
+     *
+     * @param clientId
+     * @param messageId
+     */
+    public void remove(String clientId, Integer messageId) {
+        redisTemplate.opsForHash().delete(CACHE_PRE + clientId, messageId);
+    }
+
+
+    /***
+     * 删除
+     * @param clientId
+     */
+    public void remove(String clientId) {
+        redisTemplate.delete(CACHE_PRE + clientId);
     }
 
 
