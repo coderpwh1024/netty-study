2eaa0ea61f2db5e77b23694381cd92f64c8d4949
pengwenhao <pengwenhao@qnvip.cn>
2023-12-27 18:44:20 +0800

    feat:init DupPublishMessageStoreServiceImpl

---
+++ netty-lot/src/main/java/com/coderpwh/lot/store/message/DupPublishMessageStoreServiceImpl.java
@@ -0,0 +1,63 @@
+package com.coderpwh.lot.store.message;
+
+import com.coderpwh.lot.common.message.DupPubRelMessageStore;
+import com.coderpwh.lot.common.message.DupPublishMessageStore;
+import com.coderpwh.lot.common.message.GrozaDupPublishMessageStoreService;
+import com.coderpwh.lot.store.cache.GrozaDupPublishMessageCache;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author coderpwh
+ */
+@Service
+public class DupPublishMessageStoreServiceImpl implements GrozaDupPublishMessageStoreService {
+
+
+    @Autowired
+    private GrozaDupPublishMessageCache grozaDupPublishMessageCache;
+
+
+    /***
+     * 存放
+     * @param clientId
+     * @param dupPublishMessageStore
+     */
+    @Override
+    public void put(String clientId, DupPublishMessageStore dupPublishMessageStore) {
+        grozaDupPublishMessageCache.put(clientId, dupPublishMessageStore.getMessageId(), dupPublishMessageStore);
+    }
+
+
+    /***
+     *  获取详情
+     * @param clientId
+     * @return
+     */
+    @Override
+    public List<DupPublishMessageStore> get(String clientId) {
+        if (grozaDupPublishMessageCache.containsKey(clientId)) {
+            ConcurrentHashMap<Integer, DupPublishMessageStore> map = grozaDupPublishMessageCache.get(clientId);
+            Collection<DupPublishMessageStore> collection = map.values();
+            return new ArrayList<>(collection);
+        }
+        return new ArrayList<>();
+    }
+
+    @Override
+    public void remove(String clientId, int messageId) {
+
+    }
+
+    @Override
+    public void removeByClient(String clientId) {
+
+    }
+
+
+}
