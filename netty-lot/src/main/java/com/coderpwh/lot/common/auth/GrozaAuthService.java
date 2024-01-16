6a0ce2d3bcfd3d6e5c146d99f86639592fcd6e5e
coderpwh <pengwenhao@aliyun.com>
2024-01-16 16:28:44 +0800

    feat:init Connect

--- netty-lot/src/main/java/com/coderpwh/lot/common/auth/GrozaAuthService.java
+++ netty-lot/src/main/java/com/coderpwh/lot/common/auth/GrozaAuthService.java
@@ -1,5 +1,8 @@
 package com.coderpwh.lot.common.auth;
 
+/**
+ * @author coderpwh
+ */
 public interface GrozaAuthService {
     /**
      * 验证用户名和密码是否正确
---
+++ netty-lot/src/main/java/com/coderpwh/lot/protocol/Connect.java
@@ -0,0 +1,39 @@
+package com.coderpwh.lot.protocol;
+
+import com.coderpwh.lot.common.auth.GrozaAuthService;
+import com.coderpwh.lot.common.message.GrozaDupPubRelMessageStoreService;
+import com.coderpwh.lot.common.message.GrozaDupPublishMessageStoreService;
+import com.coderpwh.lot.common.session.GrozaSessionStoreService;
+import com.coderpwh.lot.common.subscribe.GrozaSubscribeStoreService;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author coderpwh
+ */
+@Slf4j
+public class Connect {
+
+    private GrozaAuthService grozaAuthService;
+
+    private GrozaSessionStoreService grozaSessionStoreService;
+
+    private GrozaDupPublishMessageStoreService grozaDupPublishMessageStoreService;
+
+    private GrozaDupPubRelMessageStoreService grozaDupPubRelMessageStoreService;
+
+    private GrozaSubscribeStoreService grozaSubscribeStoreService;
+
+    public Connect(GrozaAuthService grozaAuthService,
+                   GrozaSessionStoreService grozaSessionStoreService,
+                   GrozaDupPublishMessageStoreService grozaDupPublishMessageStoreService,
+                   GrozaDupPubRelMessageStoreService grozaDupPubRelMessageStoreService,
+                   GrozaSubscribeStoreService grozaSubscribeStoreService) {
+        this.grozaAuthService = grozaAuthService;
+        this.grozaSessionStoreService = grozaSessionStoreService;
+        this.grozaDupPublishMessageStoreService = grozaDupPublishMessageStoreService;
+        this.grozaDupPubRelMessageStoreService = grozaDupPubRelMessageStoreService;
+        this.grozaSubscribeStoreService = grozaSubscribeStoreService;
+    }
+
+
+}
