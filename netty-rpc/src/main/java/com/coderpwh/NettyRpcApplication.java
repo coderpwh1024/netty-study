47819c829577c84632344e72fb15d56b80fe6702
coderpwh <pengwenhao@aliyun.com>
2024-01-18 13:32:01 +0800

    feat:init NettyRpcApplication

---
+++ netty-rpc/src/main/java/com/coderpwh/NettyRpcApplication.java
@@ -0,0 +1,15 @@
+package com.coderpwh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author coderpwh
+ */
+@SpringBootApplication
+public class NettyRpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyRpcApplication.class, args);
+    }
+}
